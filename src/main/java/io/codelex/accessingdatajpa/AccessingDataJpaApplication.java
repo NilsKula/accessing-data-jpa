5fff2233864636ada9bac46bc89128f6db7fc9a2
nils <kula.nils@gmail.com>
2019-04-26 14:44:19 +0300

    finished

--- src/main/java/io/codelex/accessingdatajpa/AccessingDataJpaApplication.java
+++ src/main/java/io/codelex/accessingdatajpa/AccessingDataJpaApplication.java
@@ -1,13 +1,50 @@
 package io.codelex.accessingdatajpa;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AccessingDataJpaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AccessingDataJpaApplication.class, args);
-	}
+    private static final Logger log = LoggerFactory.getLogger(AccessingDataJpaApplication.class);
 
+    public static void main(String[] args) {
+        SpringApplication.run(AccessingDataJpaApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner demo(CustomerRepository repository) {
+        return (args) -> {
+            repository.save(new Customer("Jack", "Bauer"));
+            repository.save(new Customer("Chloe", "Bauer"));
+            repository.save(new Customer("Kim", "Palmer"));
+            repository.save(new Customer("David", "Palmer"));
+            repository.save(new Customer("Michelle", "Dessler"));
+
+            log.info("Customers found with findAll():");
+            log.info("-------------------------------");
+            for (Customer customer : repository.findAll()) {
+                log.info(customer.toString());
+            }
+            log.info("");
+
+            repository.findById(1L)
+                    .ifPresent(customer -> {
+                        log.info("Customer found with findById(1L):");
+                        log.info("-------------------------------");
+                        log.info(customer.toString());
+                        log.info("");
+                    });
+
+            log.info("Customer found with findByLastName('Bauer'):");
+            log.info("--------------------------------------------");
+            repository.findByLastName("Bauer").forEach(bauer -> {
+                log.info(bauer.toString());
+            });
+        };
+    }
 }
